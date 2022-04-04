e58c08fcb5bcb93b8398539ba9a44c419af22eed
herteleo <leonard.hertel@gmail.com>
2022-04-04 07:20:13 +0200

    fix: correct pwa short name

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig(({ mode }) => {
         includeAssets: ['favicon.png'],
         manifest: {
           name: displayName,
+          short_name: displayName,
           background_color: colors.gray['900'],
           theme_color: '#04060a',
           icons: [
