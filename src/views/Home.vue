6e456d7ba9445f0f1955697c4d05bddfbe9e23d4
herteleo <leonard.hertel@gmail.com>
2022-11-02 20:32:14 +0100

    feat: improve browse button design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,10 +8,10 @@ useDir();
 <template>
   <div class="grid grow place-items-center">
     <div class="max-w-md space-y-2 p-4 text-center text-gray-400">
-      <button @click="setRootDir">
+      <button class="rounded-xl border border-slate-800 p-6 hover:bg-slate-800" @click="setRootDir">
         <div class="space-y-2">
           <app-icon class="mx-auto opacity-60" name="Folder" size="128" />
-          <div class="text-xl capitalize">Click to browse your local files in the browser</div>
+          <div class="text-xl capitalize">Click to browse your local files</div>
           <div class="opacity-30">Because why not. Your local files stay private.</div>
           <div v-if="!isFileSystemApiAvailable" class="pt-2 text-sm text-amber-600">
             Sadly your browser is not supported.<br />Try the latest Chrome Browser.
